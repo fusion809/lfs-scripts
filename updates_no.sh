08d7ff460f69d7258fe38df393542ed2a6d2281a
Brenton Horne <brentonhorne77@gmail.com>
2026-07-05 20:05:35 +1000

    Fixing updates_no shebang

--- updates_no.sh
+++ updates_no.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 source $HOME/.bashrc
 if ! find ~/updates.log -mmin -10 | grep -q . ; then
 	updates 2>&1 | tee ~/updates.log > /dev/null
@@ -9,4 +9,3 @@ no_files_missing=$(grep -cF "[FILES MISSING]" ~/updates.log)
 no_missing_total=$(($no_missing + $no_files_missing))
 no_failed=$(grep -cF "[FAILED]" ~/updates.log)
 echo " $no_updates 󰂕 $no_missing_total  $no_failed"
-echo "Up $no_updates M $no_missing_total F $no_failed"

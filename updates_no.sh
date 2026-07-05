720b35840a4af7907f1afc639b1dd00d6a1a9781
Brenton Horne <brentonhorne77@gmail.com>
2026-07-05 20:03:23 +1000

    ~/updates.log to ensure it persists after reboots

--- updates_no.sh
+++ updates_no.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+!/bin/bash
 source $HOME/.bashrc
-if ! find /tmp/updates.log -mmin -10 | grep -q . ; then
-	updates 2>&1 | tee /tmp/updates.log > /dev/null
+if ! find ~/updates.log -mmin -10 | grep -q . ; then
+	updates 2>&1 | tee ~/updates.log > /dev/null
 fi
-no_updates=$(grep -cF "[UPDATE]" /tmp/updates.log)
-no_missing=$(grep -cF "[MISSING]" /tmp/updates.log)
-no_files_missing=$(grep -cF "[FILES MISSING]" /tmp/updates.log)
+no_updates=$(grep -cF "[UPDATE]" ~/updates.log)
+no_missing=$(grep -cF "[MISSING]" ~/updates.log)
+no_files_missing=$(grep -cF "[FILES MISSING]" ~/updates.log)
 no_missing_total=$(($no_missing + $no_files_missing))
-no_failed=$(grep -cF "[FAILED]" /tmp/updates.log)
+no_failed=$(grep -cF "[FAILED]" ~/updates.log)
 echo " $no_updates 󰂕 $no_missing_total  $no_failed"
-#echo "Up $no_updates M $no_missing_total F $no_failed"
+echo "Up $no_updates M $no_missing_total F $no_failed"

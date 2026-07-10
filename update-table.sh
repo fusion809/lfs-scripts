71d0602ac99c6bbca8a7e4be4f07e602ef8b21d5
Brenton Horne <brentonhorne77@gmail.com>
2026-07-11 02:19:27 +1000

    Improving table formatting of update-table.sh

--- update-table.sh
+++ update-table.sh
@@ -9,5 +9,17 @@ awk -F'[[:space:]]*\\|[[:space:]]*' '
 
     match($3, /^(.*) (\[[^]]+\])$/, a)
 
-    printf "%-16s |   %-7s | %-8s %s\n", $1, $2, a[1], a[2]
+    rows[NR][1] = $1
+    rows[NR][2] = $2
+    rows[NR][3] = a[1]
+    rows[NR][4] = a[2]
+
+    if (length($1) > maxw1) maxw1 = length($1)
+    if (length($2) > maxw2) maxw2 = length($2)
+    if (length(a[1]) > maxw3) maxw3 = length(a[1])
+}
+END {
+    fmt = "%-" maxw1 "s | %-" maxw2 "s | %-" maxw3 "s %s\n"
+    for (i = 1; i <= NR; i++)
+        printf fmt, rows[i][1], rows[i][2], rows[i][3], rows[i][4]
 }'

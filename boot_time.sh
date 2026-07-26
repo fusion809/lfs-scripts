a700af7a2e618ba1357d8c17592ee891716f4fa4
Brenton Horne <brentonhorne77@gmail.com>
2026-07-26 11:47:58 +1000

    Tweaking boot_time.sh

--- boot_time.sh
+++ boot_time.sh
@@ -3,8 +3,25 @@ timestamp=$(uptime -s)
 if [[ -f ~/plots/$timestamp.svg ]]
 then
 	filename=$HOME/plots/$timestamp.svg
-else
+elif [[ -f ~/plots/outliers/$timestamp.svg ]]; then
 	filename=$HOME/plots/outliers/$timestamp.svg
+else
+	diff=1000000000
+find ~/plots -type f -name '*.svg' -print0 |
+while IFS= read -r -d '' filetmp; do
+    i=${filetmp:t:r}       # basename without .svg
+
+    plot_timestamp=$(date -d "$i" +%s)
+    curr_time=$(date -d "$timestamp" +%s)
+
+    diff_tmp=$(( plot_timestamp - curr_time ))
+    diff_tmp=$(( diff_tmp < 0 ? -diff_tmp : diff_tmp ))
+
+    if (( diff_tmp < diff )); then
+        diff=$diff_tmp
+        filename=$filetmp
+    fi
+done
 fi
 cat "$filename" | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.venv,venv} kernel | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.venv,venv} user | sed 's/.*= //g'
 

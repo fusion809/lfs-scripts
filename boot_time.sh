dbc71dc7dd6018175326b224b2b8e32aacee2234
Brenton Horne <brentonhorne77@gmail.com>
2026-07-27 18:55:00 +1000

    Tweaking boot_time.sh to show LFS version

--- boot_time.sh
+++ boot_time.sh
@@ -1,32 +1,6 @@
-#!/bin/bash
-timestamp=$(uptime -s)
+#!/bin/zsh
+source $HOME/.zshrc
+LFS_VERSION=$(cat /etc/os-release | grep "^VERSION_ID" | cut -d '"' -f 2)
 
-while ! [[ $boot_time =~ ^[0-9]+([.][0-9]+)?s ]]; do
-	if [[ -f ~/plots/$timestamp.svg ]]; then
-		filename=$HOME/plots/$timestamp.svg
-	elif [[ -f ~/plots/outliers/$timestamp.svg ]]; then
-		filename=$HOME/plots/outliers/$timestamp.svg
-	else
-		diff=1000000000
-		find ~/plots -type f -name '*.svg' -print0 |
-		while IFS= read -r -d '' filetmp; do
-	    		i=${filetmp:t:r}       # basename without .svg
-
-			plot_timestamp=$(date -d "$i" +%s)
-    			curr_time=$(date -d "$timestamp" +%s)
-
-			diff_tmp=$(( plot_timestamp - curr_time ))
-			diff_tmp=$(( diff_tmp < 0 ? -diff_tmp : diff_tmp ))
-
-			if (( diff_tmp < diff )); then
-        			diff=$diff_tmp
-        			filename=$filetmp
-    			fi
-		done
-	fi
-
-	boot_time=$(cat "$filename" | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.venv,venv} kernel | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.venv,venv} user | sed 's/.*= //g')
-done
-
-echo " $boot_time"
+echo "󰌽 $LFS_VERSION  $(boot_time)"
 

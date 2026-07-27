cab9caf5a37af8d96574ebd297af9c20af046a86
Brenton Horne <brentonhorne77@gmail.com>
2026-07-27 22:33:08 +1000

    +Package list to boot_time.sh

--- boot_time.sh
+++ boot_time.sh
@@ -2,5 +2,5 @@
 source $HOME/.zshrc
 LFS_VERSION=$(cat /etc/os-release | grep "^VERSION_ID" | cut -d '"' -f 2)
 
-echo "󰌽 $LFS_VERSION  $(boot_time)"
+echo " $(boot_time)󰌽 $LFS_VERSION 󰏖 $(~/lfs-scripts/packages_no.sh | sed 's/,//g')"
 

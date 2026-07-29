957fd5560fa52651cb30bde5ceff6927308fb312
Brenton Horne <brentonhorne77@gmail.com>
2026-07-29 12:33:45 +1000

    Restructuring packages_no_long_background.sh

--- packages_no_long_background.sh
+++ packages_no_long_background.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 function hash {
-    git -C /var/lib/$1-packages rev-parse --short HEAD
+	git -C /var/lib/$1-packages rev-parse --short HEAD
 }
-function changes {
-	! git -C /var/lib/$1-packages diff --quiet
-}
-BH=$(hash book);
-CH=$(hash custom);
 
-if [[ $BH != "$(cat ~/book_hash.log)" ]] || [[ $CH != "$(cat ~/custom_hash.log)" ]] || changes book || changes custom; then
+function stHash {
+	cat ~/$1_hash.log
+}
+
+function nopkg {
+	ls /var/lib/$1-packages | wc -l
+}
+
+function comno {
+	git -C /var/lib/$1-packages rev-list --branches master --count
+}
+
+BH=$(hash book);
+BN=$(nopkg book);
+BP=$(comno book);
+CH=$(hash custom);
+CN=$(nopkg custom);
+CP=$(comno custom); 
+
+if [[ $BH != "$(stHash book)" ]] || [[ $CH != "$(stHash custom)" ]]; then
 	pip=$(pip3 list | wc -l)
-	book=$(ls /var/lib/book-packages | wc -l)
-	custom=$(ls /var/lib/custom-packages | wc -l)
 	R=$(Rscript -e 'ip <- installed.packages(); cat(ip[,1], sep="\n")' | wc -l)
 	julia=$(julia -e 'using Pkg; Pkg.status()' | wc -l)
 	if (( $julia == 0 )); then
 		julia="1"
 	fi
-	total=$(($julia+$pip+$R+$book+$custom))
-	function comno {
-	    git -C /var/lib/$1-packages rev-list --branches master --count
-	}
-	BP=$(comno book);
-	CP=$(comno custom); 
-	echo "$total [ $book (󰊢 ${BP})  $custom (󰊢 $CP)  $julia  $pip  $R]" > ~/packages_no_long.log
+	total=$(($julia+$pip+$R+$BN+$CN))
+	echo "$total [ $BN (󰊢 ${BP})  $CN (󰊢 $CP)  $julia  $pip  $R]" > ~/packages_no_long.log
 fi
 echo "$BH" > ~/book_hash.log
 echo "$CH" > ~/custom_hash.log

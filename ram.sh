a6732172d60527e181de5acc588c9ddffcb573a3
Brenton Horne <brentonhorne77@gmail.com>
2026-08-04 08:40:35 +1000

    Splitting into ram.sh

---
+++ ram.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+totalMem=$(grep -E '^MemTotal:' /proc/meminfo | sed 's/MemTotal:.\s*//g' | sed 's/ kB//g')
+freeMem=$(grep -E '^MemFree:' /proc/meminfo | sed 's/MemFree:.\s*//g' | sed 's/ kB//g')
+usedMem=$(R -q -e "$totalMem-$freeMem" | grep "^\[1\]" | cut -d ' ' -f 2)
+ram=$(R -q -e "round(100*$usedMem/$totalMem,2)" | grep "^\[1\]" | cut -d ' ' -f 2)
+echo "$ram%"

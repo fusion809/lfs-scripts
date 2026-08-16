07475fb4c59ef911e45fbbbf2787ea692bf0c6f3
Brenton Horne <brentonhorne77@gmail.com>
2026-08-17 09:59:33 +1000

    +antigravity

---
+++ Shell/11-antigravity.sh
@@ -0,0 +1,19 @@
+function aap {
+	antigravity ~/lfs_apps
+}
+
+function adot {
+	antigravity ~/lfs_dotfiles
+}
+
+function agn {
+	antigravity ~/lfs_gnuplot
+}
+
+function alfp {
+	antigravity ~/lfs_packaging
+}
+
+function ash {
+	antigravity ~/lfs-scripts
+}

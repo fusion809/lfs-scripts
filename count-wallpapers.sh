d4cf2c96de39d104d4a3cef56b0dfea9710279fe
Brenton Horne <brentonhorne77@gmail.com>
2026-08-29 12:06:08 +1000

    Fixing no of wallpapers

--- count-wallpapers.sh
+++ count-wallpapers.sh
@@ -1,1 +1,1 @@
-no=$(cat /home/fusion809//.local/state/wallpaper_index); total=$(ls ~/wallpapers | wc -l); echo "󰸉 $no/$total"
+no=$(cat /home/fusion809/.local/state/wallpaper_index); total=$(ls ~/wallpapers | wc -l); echo "󰸉 $no/$((total-1))"

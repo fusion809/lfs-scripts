2f2e09182220762b1f3273c570c2a8ef3f70a858
Brenton Horne <brentonhorne77@gmail.com>
2026-09-03 01:02:07 +1000

    Versioning fixes

--- os_version_background.sh
+++ os_version_background.sh
@@ -3,9 +3,17 @@ current="󰌽 $(< /etc/lfs-release),$(cat /etc/blfs-release | cut -d '-' -f 2)"
 
 if [[ $(< ~/logs/os_version.log) != "$current" ]]; then
 	LFS_VERSION=$(cat /etc/lfs-release)
-	BASE=$(echo $LFS_VERSION | cut -d '-' -f 1)
+	LFS_BASE=$(echo $LFS_VERSION | cut -d '-' -f 1)
 	LFS_REV=$(echo $LFS_VERSION | cut -d '-' -f 2)
+	if [[ $LFS_REV == $LFS_BASE ]]; then
+		LFS_REV="0"
+	fi
 	BLFS_VERSION=$(cat /etc/blfs-release)
+	BLFS_BASE=$(cat /etc/blfs-release | cut -d '-' -f 1)
 	BLFS_REV=$(echo $BLFS_VERSION | cut -d '-' -f 2)
-	echo "󰌽 $BASE-$LFS_REV,$BLFS_REV" > ~/logs/os_version.log
+	if [[ $LFS_BASE == $BLFS_BASE ]]; then
+		echo "󰌽 $LFS_BASE-$LFS_REV,$BLFS_REV" > ~/logs/os_version.log
+	else
+		echo "󰌽 $LFS_BASE-$LFS_REV, $BLFS_BASE-$BLFS_REV" > ~/logs/os_version.log
+	fi
 fi

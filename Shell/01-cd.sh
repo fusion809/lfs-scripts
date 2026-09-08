9fde159466b444adbfb2172fdca4b8e0194e5b4e
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 06:49:14 +1000

    Fixing cdlfp

--- Shell/01-cd.sh
+++ Shell/01-cd.sh
@@ -46,30 +46,206 @@ function cdlfp_base {
 	cd ~/lfs_packaging/$1
 }
 function cdlfp {
-	if [[ -d ~/lfs_packaging/$1 ]]; then
-		cdlfp_base $1
-	else
-		mkdir -p ~/lfs_packaging/$1
-		cdlfp_base $1
+	if [[ -z "$1" ]]; then
+		cdlfp_base
+		return;
+	fi
+	if [[ -d "$HOME/lfs_packaging/$1" ]]; then
+		cdlfp_base "$1"
+		return;
+	fi
+	if [[ -f /var/lib/book-packages/$1 ]]; then
+		mv /var/lib/book-packages/$1 /var/lib/custom-packages
+	fi
+	mkdir -p ~/lfs_packaging/$1
+	cdlfp_base "$1"
+	if [[ "$1" == *"github"* ]] || [[ "$2" == "github" ]]; then
+		if [[ "$3" == "cmake" ]]; then
 		cat > build.sh <<EOF
 #!/bin/bash
 set -e
 name=$1
-version=
+repo=\$name/\$name
+version=\$(gh_ver \$repo)
 filename="\$name-\$version.tar.gz"
 direname="\${filename/.tar.*/}"
 if ! [[ -f \$filename ]]; then
-	wget -c
+	wget -c https://github.com/\$repo/releases/download/\$direname/\$filename
 fi
 rm -rf "\$direname"
 tar xf "\$filename"
 cd "\$direname"
+cmake_options=(-D CMAKE_INSTALL_PREFIX=/usr \
+      -D CMAKE_BUILD_TYPE=Release         \
+      -D BUILD_TESTING=OFF)
+cmaki "\${cmake_options[@]}"
+cd ../..
 rm -rf "\$filename" "\$direname"
 echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
 EOF
-		chmod +x build.sh
-		echo "build.sh created but almost empty"
+elif [[ "$3" == "meson" ]]; then
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=\$name/\$name
+version=\$(gh_ver \$repo)
+filename="\$name-\$version.tar.gz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://github.com/\$repo/releases/download/\$direname/\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+meson_options=(--prefix=/usr       \
+            --buildtype=release \
+	    -D tests=false)
+mni "\${meson_options[@]}"
+cd ../..
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
+else
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=\$name/\$name
+version=\$(gh_ver \$repo)
+filename="\$name-\$version.tar.gz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://github.com/\$repo/releases/download/\$direname/\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+cmi --prefix=/usr --disable-static
+cd ../
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
+		fi		
+	elif [[ "$2" == "cmake" ]]; then
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=\$name/\$name
+version=\$(gh_ver \$repo)
+filename="\$name-\$version.tar.gz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://github.com/\$repo/releases/download/\$direname/\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+cmake_options=(-D CMAKE_INSTALL_PREFIX=/usr \
+      -D CMAKE_BUILD_TYPE=Release         \
+      -D BUILD_TESTING=OFF)
+cmaki "\${cmake_options[@]}"
+cd ../..
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
+	elif [[ "$2" == "meson" ]]; then
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=\$name/\$name
+version=\$(gh_ver \$repo)
+filename="\$name-\$version.tar.gz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+meson_options=(--prefix=/usr       \
+            --buildtype=release \
+	    -D tests=false)
+mni "\${meson_options[@]}"
+cd ../..
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
+	elif [[ "$1" == *"gnome"* ]] || [[ "$2" == "gnome" ]]; then
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=GNOME/\$name
+version=\$(gh_ver \$repo)
+majVer=\$(echo \$version | sed -E 's/\.[0-9]+\$//g')
+filename="\$name-\$version.tar.xz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://download.gnome.org/sources/\$name/\$majVer/\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+options=(--prefix=/usr --buildtype=release)
+mni "\${options[@]}"
+cd ../..
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
+	elif [[ "$1" == *"kde"* ]] || [[ "$2" == "kde" ]]; then
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=KDE/\$name
+version=\$(gh_ver \$repo)
+majVer=\$(echo \$version | sed -E 's/\.[0-9]+\$//g')
+filename="\$name-\$version.tar.xz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://download.kde.org/stable/release-service/\$version/src/\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+options=(-D CMAKE_INSTALL_PREFIX=/usr \
+      -D CMAKE_BUILD_TYPE=Release         \
+      -D BUILD_TESTING=OFF)
+cmaki "\${options[@]}"
+cd ../..
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
+	else
+		cat > build.sh <<EOF
+#!/bin/bash
+set -e
+name=$1
+repo=\$name/\$name
+version=\$(gh_ver \$repo)
+filename="\$name-\$version.tar.gz"
+direname="\${filename/.tar.*/}"
+if ! [[ -f \$filename ]]; then
+	wget -c https://github.com/\$repo/releases/download/\$direname/\$filename
+fi
+rm -rf "\$direname"
+tar xf "\$filename"
+cd "\$direname"
+cmi --prefix=/usr --disable-static
+cd ../
+rm -rf "\$filename" "\$direname"
+echo "\$version" | sudo tee "/var/lib/custom-packages/\$name"
+EOF
 	fi
+	chmod +x build.sh
+	echo "build.sh created based on template..."
+	if [[ -f /var/lib/custom-packages/$1 ]]; then
+		add_deps "$1"
+	fi
+	vim build.sh
 }
 
 alias cdlp=cdlfp
@@ -105,4 +281,3 @@ function cdsh {
 function cdw {
 	cd ~/wallpapers
 }
-

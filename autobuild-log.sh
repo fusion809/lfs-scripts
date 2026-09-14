3ed118a0ec430c462d11d0cf9fff4e50d4315c1a
Brenton Horne <brentonhorne77@gmail.com>
2026-09-14 21:55:58 +1000

    Fixing grep build duration logging

--- autobuild-log.sh
+++ autobuild-log.sh
@@ -4,12 +4,12 @@ duration_dir="$HOME/build_duration"
 mkdir -p "$duration_dir"
 
 while :; do
-	pid=$(ps ax | grep "\.lfs_autobuild.sh" | grep -v grep | sed 's/^\s*//g' | cut -d ' ' -f 1)
+	pid=$(ps ax | grep "\.lfs_autobuild.sh" | grep -v 'grep "\.lfs_autobuild.sh"' | sed 's/^\s*//g' | cut -d ' ' -f 1)
 	if ! [[ -n $pid ]]; then
 		continue;
 	fi
 	while read -r pid; do
-		pkg=$(ps ax | grep $pid | grep -v "grep" | sed 's/.*lfs_autobuild.sh\s//g' | sed 's/ -f.*$//g')
+		pkg=$(ps ax | grep $pid | grep -v "grep $pid" | sed 's/.*lfs_autobuild.sh\s//g' | sed 's/ -f.*$//g')
 		start_time=$(ps -p $pid -o lstart=)
 		start_sec=$(date -d "$start_time" +"%s")
 		while kill -0 "$pid" 2>/dev/null; do
